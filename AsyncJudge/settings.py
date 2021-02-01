1f8f05383dccbb138ed62499e7bdda30155b7ba4
Himanshu <himanshudhiman9313@gmail.com>
2021-02-01 18:30:23 +0530

    changed

--- AsyncJudge/settings.py
+++ AsyncJudge/settings.py
@@ -96,23 +96,23 @@ CHANNEL_LAYERS = {
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'judge',
-        'USER': 'postgres',
-        'PASSWORD': 'Himanshu',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'judge',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Himanshu',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
